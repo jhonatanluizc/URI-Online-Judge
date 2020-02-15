9661fa68a9ee3c88569244851d5c1bc11fe45e6a
Jhonatan Luiz <47275727+SotnDragon@users.noreply.github.com>
2020-02-15 19:34:27 -0300

    Create 1007.c

---
+++ 1007/1007.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, c, d;
+    int dif;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
+    scanf("%d", &d);
+    dif = a*b-c*d;
+    printf("DIFERENCA = %d\n", dif);
+    return 0;
+}
