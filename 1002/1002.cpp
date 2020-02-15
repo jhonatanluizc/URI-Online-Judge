84b7020f2b99ed35207c29d15d122ed192e8e885
Jhonatan Luiz <47275727+SotnDragon@users.noreply.github.com>
2020-02-15 19:23:12 -0300

    Create 1002.cpp

---
+++ 1002/1002.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    double r;
+    cin >> r;
+    cout << fixed << setprecision(4) << "A=" << (r*r)*3.14159 << "\n";
+    return 0;
+}
